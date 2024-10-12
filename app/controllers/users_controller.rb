78d535db50c3fe8c0d6a0069fa86ef28a6aa3088
weeezik <mkihanya.33@gmail.com>
2024-10-11 22:50:50 -0700

    Edit typo in users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to users_url
     else
-      render :new, status: :unproccessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
 
